03cf7580b60e1ab24121969e0cea8ff1a15a01e8
fodela <particleme30@gmail.com>
2022-08-01 19:23:04 +0000

    test test_main.py

--- test_main.py
+++ test_main.py
@@ -12,14 +12,14 @@ TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MT
 EMAIL = 'wolf@thedoor.com'
 PASSWORD = 'huff-puff'
 
+
 @pytest.fixture
 def client():
     os.environ['JWT_SECRET'] = SECRET
-    main.APP.config['TESTING'] = True
+    main.APP.config['TESTING'] = False
     client = main.APP.test_client()
 
     yield client
-
 
 
 def test_health(client):
@@ -31,7 +31,7 @@ def test_health(client):
 def test_auth(client):
     body = {'email': EMAIL,
             'password': PASSWORD}
-    response = client.post('/auth', 
+    response = client.post('/auth',
                            data=json.dumps(body),
                            content_type='application/json')
 
